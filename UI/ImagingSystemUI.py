335557ebc4a60544bb1b0cb892fce49dbaffba6a
Andrew Toader <69984232+aet37@users.noreply.github.com>
2022-12-01 12:14:52 -0500

    Update ImagingSystemUI.py

--- UI/ImagingSystemUI.py
+++ UI/ImagingSystemUI.py
@@ -253,7 +253,6 @@ class ImagingSystem(QtWidgets.QMainWindow):
 		self.img_worker.force_stopped.connect(self.stim_thread.exit)
 		self.img_worker.finished.connect(self.stim_thread.quit)
 		self.img_worker.force_stopped.connect(self.stim_thread.quit)
-
 		self.img_worker.finished.connect(self.stim_worker.deleteLater)
 		self.img_worker.finished.connect(self.stim_thread.deleteLater)
 
@@ -266,9 +265,8 @@ class ImagingSystem(QtWidgets.QMainWindow):
 		if self.doLED:
 			self.img_worker.finished.connect(self.led_thread.exit)
 			self.img_worker.force_stopped.connect(self.led_thread.exit)
-			self.img_worker.finished.connect(self.stim_thread.terminate)
-			self.img_worker.force_stopped.connect(self.stim_thread.terminate)
-
+			self.img_worker.finished.connect(self.led_thread.quit)
+			self.img_worker.force_stopped.connect(self.led_thread.quit)
 			self.img_worker.finished.connect(self.led_worker.deleteLater)
 			self.img_worker.finished.connect(self.led_thread.deleteLater)
 
